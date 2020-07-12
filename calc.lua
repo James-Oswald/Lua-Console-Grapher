5a6a5924beed9a2448cc3cbe5500b1ca274d4a2d
James Oswald <jamesoswald111@gmail.com>
2020-07-12 03:14:46 -0400

    Add files via upload

---
+++ calc.lua
@@ -0,0 +1,25 @@
+function getx(x)
+    return -x * math.tan(0.2 * x)
+end
+width = 50
+height = 50
+a = {}
+for i = 0, height do
+    a[i] = {}
+    for j = 0, width do
+       a[i][j] = " "
+    end
+end
+for t = -100, 100, 0.01 do
+    x = math.floor(t) + (width / 2)
+    y = -math.floor(getx(t)) + (height / 2)
+    if x < height and x > 0 and y > 0 and y < width then
+        a[y][x] = "@"
+    end
+end
+for i = 0, height do
+    for j = 0, width do
+        io.write(a[i][j])
+    end  
+print()
+end
